1cfb930dbf418b7362e9966bfcf82f6b6f2d3d68
LanruoXia <122364841+LanruoXia@users.noreply.github.com>
2023-03-13 18:44:11 +0800

    Organize the project structure finish some functions of the login window

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 import javafx.application.Application;
-public class Main {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.sound.midi.SysexMessage;
+import java.io.IOException;
+
+public class Main extends Application{
+    private static Stage stage;
     public static void main(String[] args){
-        Application.launch(TestController.class);
+        launch(args);
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Main.stage = stage;
+        stage.setTitle("MovieCan");
+        changeView("view/login.fxml");
+        stage.show();
+    }
+
+    public static void changeView(String fxmlFile){
+        Parent root = null;
+        try {
+            System.out.println(Main.class.getClassLoader().getResource(fxmlFile));
+            root = FXMLLoader.load(Main.class.getClassLoader().getResource(fxmlFile));
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
