4610fea810b3a98865f6f2833ccd6439cb359848
LanruoXia <122364841+LanruoXia@users.noreply.github.com>
2023-03-23 17:12:58 +0800

    connected the database movieadmin to the system added sql file inserted some data

--- src/main/java/org/example/utils/JdbcUtils.java
+++ src/main/java/org/example/utils/JdbcUtils.java
@@ -3,10 +3,10 @@ package org.example.utils;
 import java.sql.*;
 
 public class JdbcUtils {
-    private static final String USER_NAME = "root";
-    private static final String PASSWORD = "x20030926@X";
+    private static final String USER_NAME = "user1";
+    private static final String PASSWORD = "Abc1234!";
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String URL = "jdbc:mysql://localhost:3306/movieadmin";
     private Connection connection;
 
     public JdbcUtils(){
@@ -24,7 +24,8 @@ public class JdbcUtils {
      */
     public Connection getConnection(){
         System.out.println("Connecting to the database...");
-        try(Connection conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);) {
+        try {
+            connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
             System.out.println("Connected database successfully...");
         } catch (SQLException e) {
             e.printStackTrace();
